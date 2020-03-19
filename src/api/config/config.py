37634d47383bb883609ff9468bc2f3003ea38caa
mariusmni <mariusmni@gmail.com>
2020-03-19 06:47:07 +0000

    Use RDS database in prod

--- src/api/config/config.py
+++ src/api/config/config.py
@@ -1,3 +1,5 @@
+import os
+
 class Config(object):
     DEBUG = False
     TESTING = False
@@ -17,7 +19,14 @@ class Config(object):
     UPLOAD_FOLDER = 'images'
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://marius:password@localhost:3306/proddb'
+    missing='NONE'
+    dbName = os.environ.get('RDS_DB_NAME', missing)
+    dbUser = os.environ.get('RDS_USERNAME', missing)
+    dbPassword = os.environ.get('RDS_PASSWORD', missing)
+    dbHost = os.environ.get('RDS_HOSTNAME', missing)
+    dbPort = os.environ.get('RDS_PORT', missing)
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{dbUser}:{dbPassword}@{dbHost}:{dbPort}/{dbName}'
+    print("DB URI: ", SQLALCHEMY_DATABASE_URI);
 
 class DevelopmentConfig(Config):
     DEBUG = True
